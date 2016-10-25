c7757d4462e1a1d5949e36351ae2549ef1b2a8f6
Krüpl Zsolt <hg2ecz@ham.hu>
2016-10-25 18:13:31 +0200

    import time -> utime (TNX HA5DI Béla)

--- Python-micropython/benchmark.py
+++ Python-micropython/benchmark.py
@@ -4,7 +4,7 @@
 #from __future__ import print_function
 
 import fft
-import time
+import utime
 
 LOG2FFTSIZE = 12
 FFT_REPEAT = 100
@@ -20,10 +20,10 @@ if __name__ == "__main__":
     for i in range(int(SIZE/2), SIZE):
         xy[i]=-1.;
 
-    timestart = time.time()
+    timestart = utime.time()
     for i in range(FFT_REPEAT):
         fft_out = fft.fft(LOG2FFTSIZE, xy)
-    eltime = 1000*(time.time() - timestart);
+    eltime = 1000*(utime.time() - timestart);
     print ("%6d piece of %d pt FFT;  %9.5f ms/piece\n"%(FFT_REPEAT, 1<<LOG2FFTSIZE, eltime/FFT_REPEAT))
 
     for i in range(6):
