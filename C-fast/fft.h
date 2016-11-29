570517b10e858f1df8b106b2c782ff6fe287b155
Krüpl Zsolt <hg2ecz@ham.hu>
2016-11-29 22:21:30 +0100

    +c99 "restrict"

--- C-fast/fft.h
+++ C-fast/fft.h
@@ -1,4 +1,4 @@
 #include <complex.h>
 
 // step: N*log2(N)
-void fft     (int log2point, double complex *xy_out, const double complex *xy_in);
+void fft     (int log2point, double complex *restrict xy_out, const double complex *restrict xy_in);
