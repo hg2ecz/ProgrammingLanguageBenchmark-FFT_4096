f54d35c2973d22e0ebc05a0ae02c9be332b26f15
Krüpl Zsolt <hg2ecz@ham.hu>
2017-01-08 18:53:07 +0100

    speed opt

--- C-fast_float-nocomplex-floatemu/float_emu.c
+++ C-fast_float-nocomplex-floatemu/float_emu.c
@@ -28,49 +28,47 @@ static inline float add(float ain, float bin) {
     unsigned int a_frac = (1<<23) | (*ap & ((1<<23)-1));
     unsigned int b_frac = (1<<23) | (*bp & ((1<<23)-1));
 
-    unsigned int frac;
-
     if (a_exp == 0 && a_frac == (1<<23) && b_exp == 0 && b_frac == (1<<23)) return ain; // zero
 
     if (a_sign == b_sign) {
 	if (b_exp > a_exp) {
-	    frac = (a_frac >> ((b_exp - a_exp)>>23)) + b_frac;
+	    a_frac = (a_frac >> ((b_exp - a_exp)>>23)) + b_frac;
 	    a_exp = b_exp;
 	} else if (a_exp > b_exp) {
-	    frac = a_frac + (b_frac >> ((a_exp - b_exp)>>23));
-	} else frac = a_frac + b_frac;
+	    a_frac += b_frac >> ((a_exp - b_exp)>>23);
+	} else a_frac += b_frac;
 
-	if (frac & (1<<24)) { // exponent normalisation (+)
+	if (a_frac & (1<<24)) { // exponent normalisation (+)
 	    a_exp += 1<<23;
-	    frac>>=1;
+	    a_frac>>=1;
 	}
     } else { // (+ and -) or (- and +)
 	if (b_exp > a_exp) {
 	    a_sign = b_sign;
-	    frac = b_frac - (a_frac >> ((b_exp - a_exp)>>23));
+	    a_frac = b_frac - (a_frac >> ((b_exp - a_exp)>>23));
 	    a_exp = b_exp;
 	} else if (a_exp > b_exp) {
-	    frac = a_frac - (b_frac >> ((a_exp - b_exp)>>23));
+	    a_frac -= b_frac >> ((a_exp - b_exp)>>23);
 	} else {
 	    if (a_frac >= b_frac) {
-		frac = a_frac - b_frac;
+		a_frac -= b_frac;
 	    } else {
 		a_sign = b_sign;
-		frac = b_frac - a_frac;
+		a_frac = b_frac - a_frac;
 	    }
 	}
-	if (!(frac & ((1>>23)-1))) { // zero --> return zero
+	if (!(a_frac & ((1>>23)-1))) { // zero --> return zero
 	    float *resf = (float *)&a_sign;
 	    return *resf;
 	}
 	// fast exponent normalisation (-)
-	if (!(frac & (0xffff<<8))){ frac <<=16; a_exp -=16<<23; } //16 bit group
-	if (!(frac & (0xff<<16))) { frac <<= 8; a_exp -= 8<<23; } // 8 bit group
-	if (!(frac & (0x0f<<20))) { frac <<= 4; a_exp -= 4<<23; } // 4 bit group
-	if (!(frac & (0x03<<22))) { frac <<= 2; a_exp -= 2<<23; } // 2 bit group
-	if (!(frac & (0x01<<23))) { frac <<= 1; a_exp -= 1<<23; } // 1 bit
+	if (!(a_frac & (0xffff<<8))){ a_frac <<=16; a_exp -=16<<23; } //16 bit group
+	if (!(a_frac & (0xff<<16))) { a_frac <<= 8; a_exp -= 8<<23; } // 8 bit group
+	if (!(a_frac & (0x0f<<20))) { a_frac <<= 4; a_exp -= 4<<23; } // 4 bit group
+	if (!(a_frac & (0x03<<22))) { a_frac <<= 2; a_exp -= 2<<23; } // 2 bit group
+	if (!(a_frac & (0x01<<23))) { a_frac <<= 1; a_exp -= 1<<23; } // 1 bit
     }
-    unsigned int res = a_sign | a_exp | (frac & ((1<<23)-1));
+    unsigned int res = a_sign | a_exp | (a_frac & ((1<<23)-1));
     float *resf = (float*)&res;
     return *resf;
 }
