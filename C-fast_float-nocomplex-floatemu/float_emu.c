fbea86984c54ecd292be2fd343d9076b8a6fb923
Krüpl Zsolt <hg2ecz@ham.hu>
2017-01-08 17:24:25 +0100

    speed opt

--- C-fast_float-nocomplex-floatemu/float_emu.c
+++ C-fast_float-nocomplex-floatemu/float_emu.c
@@ -6,11 +6,13 @@
    You can simulate speed CPU without FPU (many microcontrollers)
 
    (C) 2016  Zsolt Krüpl <hg2ecz@ham.hu>
+
+Functions:
+    float add(float ain, float bin);
+    float sub(float ain, float bin);
+    float mul(float ain, float bin);
 */
 
-static inline float add(float ain, float bin);
-static inline float sub(float ain, float bin);
-static inline float mul(float ain, float bin);
 
 // 1 sign + 8 exp + 23 mant
 static inline float add(float ain, float bin) {
@@ -20,8 +22,8 @@ static inline float add(float ain, float bin) {
     unsigned int a_sign = *ap & (1U<<31);
     unsigned int b_sign = *bp & (1U<<31);
 
-    int a_exp = ((*ap & ((1U<<31)-1)) >> 23)-127;
-    int b_exp = ((*bp & ((1U<<31)-1)) >> 23)-127;
+    int a_exp = *ap & (0xff<<23);
+    int b_exp = *bp & (0xff<<23);
 
     unsigned int a_frac = (1<<23) | (*ap & ((1<<23)-1));
     unsigned int b_frac = (1<<23) | (*bp & ((1<<23)-1));
@@ -30,32 +32,32 @@ static inline float add(float ain, float bin) {
     int exp;
     unsigned int frac;
 
-    if (a_exp == -127 && a_frac == (1<<23) && b_exp == -127 && b_frac == (1<<23)) return ain; // zero
+    if (a_exp == 0 && a_frac == (1<<23) && b_exp == 0 && b_frac == (1<<23)) return ain; // zero
 
     if (a_sign == b_sign) {
 	sign = a_sign;
 	exp = a_exp;
 	if (b_exp > a_exp) {
 	    exp = b_exp;
-	    a_frac >>= b_exp - a_exp;
+	    a_frac >>= (b_exp - a_exp)>>23;
 	} else if (a_exp > b_exp) {
-	    b_frac >>= a_exp - b_exp;
+	    b_frac >>= (a_exp - b_exp)>>23;
 	}
 	frac = a_frac + b_frac;
 	if (frac & (1<<24)) {
-	    exp++;
+	    exp += 1<<23;
 	    frac>>=1;
 	}
     } else { // (+ and -) or (- and +)
 	if (b_exp > a_exp) {
 	    sign = b_sign;
 	    exp = b_exp;
-	    a_frac >>= b_exp - a_exp;
+	    a_frac >>= (b_exp - a_exp)>>23;
 	    frac = b_frac - a_frac;
 	} else if (a_exp > b_exp) {
 	    sign = a_sign;
 	    exp = a_exp;
-	    b_frac >>= a_exp - b_exp;
+	    b_frac >>= (a_exp - b_exp)>>23;
 	    frac = a_frac - b_frac;
 	} else {
 	    exp = a_exp;
@@ -70,10 +72,10 @@ static inline float add(float ain, float bin) {
 	for (int i=0; i<23; i++) {
 	    if (frac & (1<<23)) break;
 	    frac <<=1;
-	    exp--;
+	    exp -= 1<<23;
 	}
     }
-    unsigned int er = sign | ((exp+127) << 23) | (frac & ((1<<23)-1));
+    unsigned int er = sign | exp | (frac & ((1<<23)-1));
     float *erf = (float*)&er;
     return *erf;
 }
@@ -90,24 +92,24 @@ static inline float mul(float ain, float bin) {
     unsigned int *ap = (unsigned int *)&ain;
     unsigned int *bp = (unsigned int *)&bin;
 
-    unsigned int a_sign = *ap & (1U<<31);
-    unsigned int b_sign = *bp & (1U<<31);
-
-    int a_exp = ((*ap & ((1U<<31)-1)) >> 23)-127;
-    int b_exp = ((*bp & ((1U<<31)-1)) >> 23)-127;
+    unsigned int sign = (*ap & (1U<<31)) ^ (*bp & (1U<<31));        // neg * neg = pos, ...
+    int exp = (*ap & (0xff<<23)) + (*bp & (0xff<<23)) - (0x7f<<23); // exp + exp - 127
+    if (exp < 0) {
+	float *resf = (float *)&sign;
+	return *resf;
+    }
 
     unsigned int a_frac = (1<<23) | (*ap & ((1<<23)-1));
     unsigned int b_frac = (1<<23) | (*bp & ((1<<23)-1));
 
-    unsigned int frac = ( (long long)a_frac * b_frac) >> 23;
-    if (frac & (1<<24)) {
-	a_exp++;
-	frac>>=1;
+    a_frac = ( (long long)a_frac * b_frac) >> 23;
+    if (a_frac & (1<<24)) {
+	exp += 1<<23;  // time opt
+	a_frac>>=1;
     }
 
-    int exp = a_exp+b_exp+127;
-    if (exp > 255) exp = 255;
-    unsigned int er = exp < 0 ? (a_sign^b_sign) : (a_sign^b_sign) | (exp << 23) | (frac & ((1<<23)-1));
-    float *erf = (float*)&er;
-    return *erf;
+    //if (exp > (0xff<<23)) exp = 0xff<<23; ---> not need for signal processing
+    unsigned int res = exp < 0 ? sign : sign | exp | (a_frac & ((1<<23)-1));
+    float *resf = (float*)&res;
+    return *resf;
 }
