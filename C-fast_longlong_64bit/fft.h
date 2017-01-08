e28e2bf242752a45c7769ea3a5968d53e0487b3e
Krüpl Zsolt <hg2ecz@ham.hu>
2017-01-08 23:34:00 +0100

    +int64

--- C-fast_longlong_64bit/fft.h
+++ C-fast_longlong_64bit/fft.h
@@ -2,8 +2,8 @@
 #define INTMULSAMPLE (55-4)
 
 struct _sample {
-    long i;
-    long q;
+    long long i;
+    long long q;
 };
 // step: N*log2(N)
 void fft     (int log2point, struct _sample *restrict xy_out, const struct _sample *restrict xy_in);
