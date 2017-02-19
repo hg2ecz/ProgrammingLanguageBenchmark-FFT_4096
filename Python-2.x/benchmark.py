b503cb658e886e427978b522931d3e598a77280e
Krüpl Zsolt <hg2ecz@ham.hu>
2017-02-19 06:44:34 +0100

    without ';'

--- Python-2.x/benchmark.py
+++ Python-2.x/benchmark.py
@@ -15,15 +15,15 @@ xy = [0+0j]*SIZE
 if __name__ == "__main__":
 
     for i in xrange(int(SIZE/2)):
-        xy[i]= 1.;
+        xy[i]= 1.
 
     for i in xrange(int(SIZE/2), SIZE):
-        xy[i]=-1.;
+        xy[i]=-1.
 
     timestart = time.time()
     for i in xrange(FFT_REPEAT):
         fft_out = fft.fft(LOG2FFTSIZE, xy)
-    eltime = 1000*(time.time() - timestart);
+    eltime = 1000*(time.time() - timestart)
     print ("%6d piece of %d pt FFT;  %9.5f ms/piece\n"%(FFT_REPEAT, 1<<LOG2FFTSIZE, eltime/FFT_REPEAT))
 
     for i in xrange(6):
--- Python-micropython/benchmark.py
+++ Python-micropython/benchmark.py
@@ -15,15 +15,15 @@ xy = [0+0j]*SIZE
 if __name__ == "__main__":
 
     for i in range(int(SIZE/2)):
-        xy[i]= 1.;
+        xy[i]= 1.
 
     for i in range(int(SIZE/2), SIZE):
-        xy[i]=-1.;
+        xy[i]=-1.
 
     timestart = utime.time()
     for i in range(FFT_REPEAT):
         fft_out = fft.fft(LOG2FFTSIZE, xy)
-    eltime = 1000*(utime.time() - timestart);
+    eltime = 1000*(utime.time() - timestart)
     print ("%6d piece of %d pt FFT;  %9.5f ms/piece\n"%(FFT_REPEAT, 1<<LOG2FFTSIZE, eltime/FFT_REPEAT))
 
     for i in range(6):
