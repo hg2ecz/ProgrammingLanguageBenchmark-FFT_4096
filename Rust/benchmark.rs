84d54f0b9833b1c63cec7ea617f34f4a8c823e6f
Krüpl Zsolt <hg2ecz@ham.hu>
2017-02-21 15:26:39 +0100

    +speed

--- Rust/benchmark.rs
+++ Rust/benchmark.rs
@@ -6,17 +6,19 @@ const LOG2FFTSIZE: u32 = 12;
 const FFT_REPEAT: u32 = 1000;
 
 const SIZE: usize = (1<<LOG2FFTSIZE);
+static mut XY         : [[f64; 2]; SIZE] = [[0.0; 2]; SIZE];
+static mut XY_OUT_FFT : [[f64; 2]; SIZE] = [[0.0; 2]; SIZE];
 
 fn main() {
-    let mut xy: [[f64; 2]; SIZE] = [[0.0; 2]; SIZE];
-    let mut xy_out_fft = [[0.0; 2]; SIZE];
 
-    for i in 0..SIZE/2    { xy[i] = [ 1.0, 0.0]; }
-    for i in SIZE/2..SIZE { xy[i] = [-1.0, 0.0]; }
+    unsafe {
+      for i in 0..SIZE/2    { XY[i] = [ 1.0, 0.0]; }
+      for i in SIZE/2..SIZE { XY[i] = [-1.0, 0.0]; }
+    }
 
 // FFT
     let start_time = Instant::now();
-    for _i in 0..FFT_REPEAT { unsafe { xy_out_fft = fft::fft(LOG2FFTSIZE, xy) } }
+    for _i in 0..FFT_REPEAT { unsafe {  fft::fft(LOG2FFTSIZE, &mut XY_OUT_FFT, XY) } }
     let elapsed_time = start_time.elapsed();
     let milliseconds = (elapsed_time.as_secs() as f64 * 1000.0) +
 		       (elapsed_time.subsec_nanos() as f64 / 1_000_000.0);
@@ -24,6 +26,6 @@ fn main() {
     println!("{} piece(s) of {} pt FFT;    {} ms/piece\n\n", FFT_REPEAT, SIZE, milliseconds/FFT_REPEAT as f64);
 
     for i in 0..6 {
-	println!("{}  {} {}", i, xy_out_fft[i][0], xy_out_fft[i][1])
+	unsafe { println!("{}  {} {}", i, XY_OUT_FFT[i][0], XY_OUT_FFT[i][1]) }
     }
 }
