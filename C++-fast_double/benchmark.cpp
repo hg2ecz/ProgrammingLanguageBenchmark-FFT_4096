a16bbfe87878f4135830aa782f86186595140b89
Krüpl Zsolt <hg2ecz@ham.hu>
2017-03-12 02:52:09 +0100

    C cleanup

--- C++-fast_double/benchmark.cpp
+++ C++-fast_double/benchmark.cpp
@@ -1,53 +1,30 @@
+#include <time.h>
 #include <iostream>
 #include "fft.h"
-
-#include <time.h>
-static struct timespec gstart;
-
-static void timestart() {
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &gstart);
-}
-
-static double timeend() {
-    struct timespec gend;
-    unsigned long long nsec;
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &gend);
-
-    nsec = 1000ULL*1000*1000*(gend.tv_sec - gstart.tv_sec) + gend.tv_nsec - gstart.tv_nsec;
-    return nsec/1000./1000.;
-}
-
-void printres(int i, std::complex<double> *d) {
-    printf("%3d %16.9f %16.9f %16.9f\n", i, d[i].real(), d[i].imag(), abs(d[i]));
-}
 
 #define SIZE (1<<LOG2FFTSIZE)
 
 std::complex<double> xy[SIZE];
-std::complex<double> xy_out_fft[SIZE];
-
-// dft_ LOGSIZE --> dft_12
-//#define FUNCTION_NAME_MAKE(name, num)  name ## num
-//#define FUNCTION_NAME(name, num)  FUNCTION_NAME_MAKE(name, num)
+std::complex<double> xy_out[SIZE];
 
 int main() {
     int i;
     double eltime;
-    printf("\n");
+    struct timespec gstart, gend;
 
     for(i=0; i<SIZE/2; i++) xy[i]= 1.;
     for(   ; i<SIZE  ; i++) xy[i]=-1.;
 
-
 // FFT
-    timestart();
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &gstart);
     fft *ff = new fft();
-    for (i=0; i<FFT_REPEAT; i++) ff->calc(LOG2FFTSIZE, xy_out_fft, xy);
-    eltime = timeend();
-    printf("%6d piece(s) of %d pt FFT;  %9.5f ms/piece\n", FFT_REPEAT, 1<<LOG2FFTSIZE, eltime/FFT_REPEAT);
+    for (i=0; i<FFT_REPEAT; i++) ff->calc(LOG2FFTSIZE, xy_out, xy);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &gend);
+    eltime = 1000.*(gend.tv_sec - gstart.tv_sec) + (gend.tv_nsec - gstart.tv_nsec)/1000000.;
+    printf("\n%6d piece(s) of %d pt FFT;  %9.5f ms/piece\n", FFT_REPEAT, 1<<LOG2FFTSIZE, eltime/FFT_REPEAT);
 
     for(i=0; i<6; i++) {
-	printres(i, xy_out_fft);
+	printf("%3d %16.9f %16.9f %16.9f\n", i, xy_out[i].real(), xy_out[i].imag(), abs(xy_out[i]));
     }
     return 0;
 }
