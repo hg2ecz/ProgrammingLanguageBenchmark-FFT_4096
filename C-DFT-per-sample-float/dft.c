6fb33407b8f86b99e319bb1393ce39e5790d00c2
Krüpl Zsolt <hg2ecz@ham.hu>
2017-06-17 08:49:03 +0200

    speed opt.

--- C-DFT-per-sample-float/dft.c
+++ C-DFT-per-sample-float/dft.c
@@ -4,34 +4,33 @@
 static int dft_point = 0;
 static float dft_revpt;
 
-static double complex phasediffXY;
-static float complex vfoXY[1<<LOG2FFTSIZE];
+static double complex dft_phasediffXY;
+static float complex dft_vfoXY[1<<LOG2FFTSIZE];
 
 
 void dft_init(int point) {
     dft_point = point;
     dft_revpt = 1./point;
     //phasediffXY = cos(-2*M_PI/point) + I*sin(-2*M_PI/point);
-    phasediffXY = cexp(2j*M_PI/point);
+    dft_phasediffXY = cexp(-2j*M_PI/point);
 
-    for (int i=0; i< 1<<LOG2FFTSIZE; i++) vfoXY[i]=1.;
+    for (int i=0; i< 1<<LOG2FFTSIZE; i++) dft_vfoXY[i]=1.;
 }
 
-
+static float complex dft_corr=0;
 void dft_sample(float complex *xy_out, float complex xy_sample) {
-    // correction
-    double complex corr=0;
+    xy_sample -= dft_corr*dft_revpt;		// remove oldest value from DFT
+
+    dft_corr = 0;
     double complex phaseXY = 1.;
 
     for(int i=0; i<dft_point; i++) {
-	vfoXY[i] *= phaseXY;
-	phaseXY *= phasediffXY;
-	corr -= xy_out[i]*conj(vfoXY[i]);
-    }
-    xy_sample -= corr*dft_revpt;
+	float complex tmp = dft_vfoXY[i];
+	xy_out[i] += xy_sample*tmp;
 
-    // dft - sample
-    for(int i=0; i<dft_point; i++) {
-	xy_out[i] -= xy_sample*(float complex)vfoXY[i];
+	tmp *= phaseXY;
+	phaseXY *= dft_phasediffXY;		// oscillator new state
+	dft_corr += xy_out[i]*conjf(tmp);	// generate new prev. correction
+	dft_vfoXY[i]=tmp;
     }
 }
