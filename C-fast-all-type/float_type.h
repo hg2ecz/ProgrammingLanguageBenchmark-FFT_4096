914f01a23be888972c6908b15c3b9ec783908c42
Krüpl Zsolt <hg2ecz@ham.hu>
2017-08-23 10:35:14 +0000

    +all data types

---
+++ C-fast-all-type/float_type.h
@@ -0,0 +1,16 @@
+// quadmath only x86_64 system
+//#include <quadmath.h>
+
+//typedef _Complex _Float16   COMPLEX_TYPE;
+typedef float  complex      COMPLEX_TYPE;
+//typedef double complex      COMPLEX_TYPE;
+//typedef long double complex COMPLEX_TYPE;
+//typedef _Complex _Float128  COMPLEX_TYPE;
+//typedef complex128          COMPLEX_TYPE; // = _Complex _Float128
+
+//typedef _Complex _Float16   COMPLEX_VFO_TYPE;
+typedef float  complex      COMPLEX_VFO_TYPE;
+//typedef double complex      COMPLEX_VFO_TYPE;
+//typedef long double complex COMPLEX_VFO_TYPE;
+//typedef _Complex _Float128  COMPLEX_VFO_TYPE;
+//typedef complex128          COMPLEX_VFO_TYPE; // = _Complex _Float128
