c557893ac7a4abab92e8fe20b243dc947ce884ba
Robert FUSZENECKER <fuszenecker@users.noreply.github.com>
2017-09-10 23:43:49 +0200

    Update create-armhf-deb.sh

--- C#/create-armhf-deb.sh
+++ C#/create-armhf-deb.sh
@@ -1,2 +1,2 @@
-dotnet publish -r linux-x64 -c Release -o packaging/opt/fft-benchmark
+dotnet publish -r linux-arm -c Release -o packaging/opt/fft-benchmark
 dpkg-deb -b packaging fft-benchmark-1.0.armhf.deb
