009e217da2ae18f4aea3b14bafd77a50ec02da56
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2017-09-30 22:13:06 +0200

    Set postinst permissions

--- C#/publish-to-raspberry.cmd
+++ C#/publish-to-raspberry.cmd
@@ -24,6 +24,7 @@ echo Post-deploy setup
 echo ----------------------------------------------------------------
 
 "c:\Program Files\plink.exe" -i c:\Users\robert.fuszenecker\ssh_login.ppk fuszenecker@192.168.1.200 "chmod 755 packaging/opt/fft-benchmark/fft-benchmark"
+"c:\Program Files\plink.exe" -i c:\Users\robert.fuszenecker\ssh_login.ppk fuszenecker@192.168.1.200 "chmod 755 packaging/opt/DEBIAN/postinst"
 
 echo ----------------------------------------------------------------
 echo Creating packages
