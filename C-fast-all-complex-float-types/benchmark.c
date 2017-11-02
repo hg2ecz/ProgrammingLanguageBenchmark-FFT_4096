9c994957db3448d35233b1f1d211fe2398dde01c
Zsolt Krüpl <hg2ecz@ham.hu>
2017-11-02 15:58:57 +0000

    +checkfft

--- C-fast-all-complex-float-types/benchmark.c
+++ C-fast-all-complex-float-types/benchmark.c
@@ -1,7 +1,10 @@
 #include <time.h>
 #include <stdio.h>
 #include "fft.h"
-#include "dft_test.h"
+
+#ifdef CHECKFFT
+# include "dft_test.h"
+#endif
 
 #define SIZE (1<<LOG2FFTSIZE)
 
@@ -24,7 +27,9 @@ int main() {
     eltime = 1000.0*(gend.tv_sec - gstart.tv_sec) + (gend.tv_nsec - gstart.tv_nsec)/1000000.;
     printf("\n%6d piece(s) of %d pt FFT;  %9.5f ms/piece\n", FFT_REPEAT, 1<<LOG2FFTSIZE, eltime/FFT_REPEAT);
 
+#ifdef CHECKFFT
     dft_test(xy_out, xy, LOG2FFTSIZE, 1e-6); // compare & relative error
+#endif
     for(i=0; i<6; i++) {
 	printf("%3d %16.4f %16.4f\n", i, creal(xy_out[i]), cimag(xy_out[i]));
     }
