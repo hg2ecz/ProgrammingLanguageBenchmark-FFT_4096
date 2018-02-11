646f0cdf78da1cc6d18318374b2ef3c349b0bc63
Zsolt Krüpl <hg2ecz@ham.hu>
2018-02-11 12:37:17 +0000

    +multithread

--- C-fast-all-complex-float-types-opt-4x-multithread/fft.h
+++ C-fast-all-complex-float-types-opt-4x-multithread/fft.h
@@ -1,6 +1,1 @@
-#include <complex.h>
-#include "float_type.h"
-
-// step: N*log2(N)
-void fft_init();
-void fft     (int log2point, COMPLEX_TYPE *restrict xy_out, const COMPLEX_TYPE *restrict xy_in);
+../C-fast-all-complex-float-types-multithread/fft.h
