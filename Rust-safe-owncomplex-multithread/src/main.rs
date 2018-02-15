389862d9852ad3883ce1a03b76e9548efcfb32ac
Róbert Fuszenecker <robert.fuszenecker@outlook.com>
2018-02-15 20:47:01 +0100

    Rust FMT

--- Rust-safe-owncomplex-multithread/src/main.rs
+++ Rust-safe-owncomplex-multithread/src/main.rs
@@ -6,28 +6,34 @@ use std::thread;
 const LOG2FFTSIZE: u32 = 12;
 const FFT_REPEAT: u32 = 1000;
 
-const SIZE: usize = (1<<LOG2FFTSIZE);
+const SIZE: usize = (1 << LOG2FFTSIZE);
 const MAXTHREAD: usize = 64;
 
 fn main() {
-    let mut xy         = Vec::new();
+    let mut xy = Vec::new();
 
     for _i in 0..MAXTHREAD {
-        xy.push([fft::Cplx {re: 1.0, im: 0.0}; SIZE]);
+        xy.push([fft::Cplx { re: 1.0, im: 0.0 }; SIZE]);
     }
 
     let mut xy_out_fft = Vec::new();
 
     for _i in 0..MAXTHREAD {
-        xy_out_fft.push([fft::Cplx {re: 1.0, im: 0.0}; SIZE]);
+        xy_out_fft.push([fft::Cplx { re: 1.0, im: 0.0 }; SIZE]);
     }
 
     for j in 0..MAXTHREAD {
-        for i in 0..SIZE/2    { xy[j][i].re =  1.0; xy[j][i].im = 0.0; }
-        for i in SIZE/2..SIZE { xy[j][i].re = -1.0; xy[j][i].im = 0.0; }
+        for i in 0..SIZE / 2 {
+            xy[j][i].re = 1.0;
+            xy[j][i].im = 0.0;
+        }
+        for i in SIZE / 2..SIZE {
+            xy[j][i].re = -1.0;
+            xy[j][i].im = 0.0;
+        }
     }
 
-// FFT
+    // FFT
     let start_time = Instant::now();
 
     let num_cores = 4;
@@ -37,9 +43,9 @@ fn main() {
         let xy_slice: [fft::Cplx; SIZE] = xy[tid as usize].clone();
 
         children.push(thread::spawn(move || -> (u32, [fft::Cplx; SIZE]) {
-            let mut xy_out_slice : [fft::Cplx; SIZE] = [fft::Cplx {re: 1.0, im: 0.0}; SIZE];
+            let mut xy_out_slice: [fft::Cplx; SIZE] = [fft::Cplx { re: 1.0, im: 0.0 }; SIZE];
             let f = fft::Fft::new();
-            for _i in 0..FFT_REPEAT/num_cores {
+            for _i in 0..FFT_REPEAT / num_cores {
                 f.fft(LOG2FFTSIZE, &mut xy_out_slice, &xy_slice);
             }
             return (tid, xy_out_slice);
@@ -54,26 +60,31 @@ fn main() {
             xy_out_fft[tid as usize][i].re = xyout[i].re;
             xy_out_fft[tid as usize][i].im = xyout[i].im;
         }
-        
+
         println!("{} {}", xy_out_fft[0][1].re, xy_out_fft[0][1].im);
     }
 
     let elapsed_time = start_time.elapsed();
-    let milliseconds = (elapsed_time.as_secs() as f64 * 1000.0) +
-		       (elapsed_time.subsec_nanos() as f64 / 1_000_000.0);
+    let milliseconds = (elapsed_time.as_secs() as f64 * 1000.0)
+        + (elapsed_time.subsec_nanos() as f64 / 1_000_000.0);
 
-    println!("{} piece(s) of {} pt FFT;    {} ms/piece\n\n", FFT_REPEAT, SIZE, milliseconds/FFT_REPEAT as f64);
+    println!(
+        "{} piece(s) of {} pt FFT;    {} ms/piece\n\n",
+        FFT_REPEAT,
+        SIZE,
+        milliseconds / FFT_REPEAT as f64
+    );
 
     println!("{}", xy_out_fft[0].len());
     println!("{} {}", xy_out_fft[0][0].re, xy_out_fft[0][0].im);
 
     for i in 0..6 {
-	    println!("{}  {} {}", i, xy_out_fft[0][i].re, xy_out_fft[0][i].im);
+        println!("{}  {} {}", i, xy_out_fft[0][i].re, xy_out_fft[0][i].im);
     }
 
     println!("");
 
     for i in 0..6 {
-	    println!("{}  {} {}", i, xy_out_fft[1][i].re, xy_out_fft[1][i].im);
+        println!("{}  {} {}", i, xy_out_fft[1][i].re, xy_out_fft[1][i].im);
     }
 }
