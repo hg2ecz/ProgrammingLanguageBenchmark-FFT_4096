adf9e522806a6c5000e874350f9f7a74384cd6ee
Róbert Fuszenecker <robert.fuszenecker@outlook.com>
2018-11-11 16:15:42 +0100

    Clean-up.

--- C++-fast_double/fft.cpp
+++ C++-fast_double/fft.cpp
@@ -46,7 +46,6 @@ std::vector<std::complex<double>> fft :: calc(int log2point, const std::vector<s
 		//	double complex wphase_XY = cos(theta) + sin(theta)*I;
 		std::complex<double> wphase_XY = this->phasevec[l2pt++];
 		std::complex<double> w_XY = std::complex<double>(1.0, 0.0);
-//		std::complex<double> w_XY = fft::One;
 
 		for (int m = 0; m < mmax; m++)
 		{
