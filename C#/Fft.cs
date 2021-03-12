0c5c12f260fab4c6a0b71597e0afa2d5147ae8e7
Fuszenecker Róbert <robert.fuszenecker@outlook.com>
2021-03-12 12:53:59 +0100

    Update to .NET 6.

--- C#/Fft.cs
+++ C#/Fft.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace CSharpFftDemo
 {
--- C#/Params.cs
+++ C#/Params.cs
@@ -3,6 +3,6 @@ namespace CSharpFftDemo
     public static class Params
     {
         internal const int Log2FftSize = 12;
-        internal const int FftRepeat = 1000;
+        internal const int FftRepeat = 50000;
     }
 }
