7dc74449506aca54d02bfc552a36d4775579b4f5
Fuszenecker Róbert <robert.fuszenecker@outlook.com>
2021-04-09 09:05:32 +0200

    Added Kubernetes support.

--- C#/Params.cs
+++ C#/Params.cs
@@ -3,6 +3,6 @@ namespace CSharpFftDemo
     public static class Params
     {
         internal const int Log2FftSize = 12;
-        internal const int FftRepeat = 50000;
+        internal const int FftRepeat = 20000;
     }
 }
