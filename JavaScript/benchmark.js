ab5329f1dc65b12822b95728a69f0c995f754aca
Róbert Fuszenecker <rfuszenecker@diligent.com>
2021-12-28 15:40:32 +0100

    Modernized the JavaScript solution.

--- JavaScript/benchmark.js
+++ JavaScript/benchmark.js
@@ -2,33 +2,33 @@
 
 "use strict";
 
-var fft = require('./fft');
+import { fft, initialize } from './fft.js';
 
+let LOG2FFTSIZE = 12
+let FFT_REPEAT = 1000
 
-var LOG2FFTSIZE=12
-var FFT_REPEAT=1000
-
-var xy = []
-var xy_out = []
+let xy = []
+let xy_out = []
 
 function main() {
-    var i
-    var eltime
-    var SIZE=1<<LOG2FFTSIZE
+    let SIZE = 1 << LOG2FFTSIZE
 
-    for(i=0; i<SIZE/2; i++) xy[i]= [1., 0]
-    for(   ; i<SIZE  ; i++) xy[i]=[-1., 0]
+    for (var i = 0; i < SIZE / 2; i++) xy[i] = [1., 0]
+    for (; i < SIZE; i++) xy[i] = [-1., 0]
+    initialize()
 
-// FFT
-    var starttime = new Date()
-    for (i=0; i<FFT_REPEAT; i++) xy_out = fft.fft(LOG2FFTSIZE, xy)
-    var endtime = new Date()
-    eltime = endtime - starttime; // msec
-    console.log("\n--> " + FFT_REPEAT + " piece of "+ (1<<LOG2FFTSIZE)+" pt FFT;  "+ (eltime/FFT_REPEAT)+" ms/piece")
+    // FFT
+    let starttime = new Date()
+    for (i = 0; i < FFT_REPEAT; i++) xy_out = fft(LOG2FFTSIZE, xy)
 
-    for(i=0; i<6; i++) {
-        console.log(i + "\t"+xy_out[i][0]+"\t"+xy_out[i][1])
+    let endtime = new Date()
+    let eltime = endtime - starttime; // msec
+    console.log("\n--> " + FFT_REPEAT + " piece of " + (1 << LOG2FFTSIZE) + " pt FFT;  " + (eltime / FFT_REPEAT) + " ms/piece")
+
+    for (i = 0; i < 6; i++) {
+        console.log(i + "\t" + xy_out[i][0] + "\t" + xy_out[i][1])
     }
+
     return 0
 }
 
--- JavaScript/fft.js
+++ JavaScript/fft.js
@@ -1,64 +1,63 @@
 "use strict"
 
 // Internal variables
-var phasevec_exist = 0
-var phasevec = []
+let phasevec = []
+
+export function initialize() {
+    for (let i = 0; i < 32; i++) {
+        let point = 2 << i
+        phasevec[i] = [Math.cos(-2 * Math.PI / point), Math.sin(-2 * Math.PI / point)]
+    }
+}
 
 // Public function
-module.exports = {
-  fft: function (log2point, xy_in) {
-    var i
-    if (!phasevec_exist) {
-	for (i=0; i<32; i++) {
-	    var point = 2<<i
-	    phasevec[i] = [ Math.cos(-2*Math.PI/point), Math.sin(-2*Math.PI/point) ] // complex.h ?
-	}
-	phasevec_exist = 1
-    }
+export function fft(log2point, xy_in) {
+    let xy_out = []
 
-    var xy_out = []
-    for (i=0; i < (1<<log2point); i++) {
-	var brev = i
-	brev = ((brev & 0xaaaaaaaa) >> 1) | ((brev & 0x55555555) << 1)
-	brev = ((brev & 0xcccccccc) >> 2) | ((brev & 0x33333333) << 2)
-	brev = ((brev & 0xf0f0f0f0) >> 4) | ((brev & 0x0f0f0f0f) << 4)
-	brev = ((brev & 0xff00ff00) >> 8) | ((brev & 0x00ff00ff) << 8)
-	//brev = (brev >> 16) | (brev << 16)
-
-	//brev >>= 32-log2point
-	brev >>= 16-log2point
-	xy_out[brev] = [ xy_in[i][0], xy_in[i][1] ]
+    for (let i = 0; i < (1 << log2point); i++) {
+        let brev = i
+        brev = ((brev & 0xaaaaaaaa) >> 1) | ((brev & 0x55555555) << 1)
+        brev = ((brev & 0xcccccccc) >> 2) | ((brev & 0x33333333) << 2)
+        brev = ((brev & 0xf0f0f0f0) >> 4) | ((brev & 0x0f0f0f0f) << 4)
+        brev = ((brev & 0xff00ff00) >> 8) | ((brev & 0x00ff00ff) << 8)
+        //brev = (brev >> 16) | (brev << 16)
+        //brev >>= 32-log2point
+        brev >>= 16 - log2point
+        xy_out[brev] = [xy_in[i][0], xy_in[i][1]]
     }
 
     // here begins the Danielson-Lanczos section
-    var n = 1<<log2point
-    var l2pt=0
-    var mmax=1
-    while (n>mmax) {
-	var istep = mmax<<1
+    let n = 1 << log2point
+    let l2pt = 0
+    let mmax = 1
 
-//	double theta = -2*M_PI/istep
-//	double complex wphase_XY = cos(theta) + sin(theta)*I
-	var wphase_XY = phasevec[l2pt++]
+    while (n > mmax) {
+        let istep = mmax << 1
 
-	var w_XY = [ 1.0, 0.0 ]
-	for (var m=0; m < mmax; m++) {
-	    for (var i=m; i < n; i += istep) {
-		var tempXY = [  w_XY[0] * xy_out[i+mmax][0] - w_XY[1] * xy_out[i+mmax][1],
-				w_XY[0] * xy_out[i+mmax][1] + w_XY[1] * xy_out[i+mmax][0] ]
+        //	double theta = -2*M_PI/istep
+        //	double complex wphase_XY = cos(theta) + sin(theta)*I
+        let wphase_XY = phasevec[l2pt++]
+        let w_XY = [1.0, 0.0]
 
-		xy_out[i+mmax][0]  = xy_out[i][0] - tempXY[0]
-		xy_out[i+mmax][1]  = xy_out[i][1] - tempXY[1]
+        for (let m = 0; m < mmax; m++) {
+            for (let i = m; i < n; i += istep) {
+                let tempXY = [w_XY[0] * xy_out[i + mmax][0] - w_XY[1] * xy_out[i + mmax][1],
+                w_XY[0] * xy_out[i + mmax][1] + w_XY[1] * xy_out[i + mmax][0]]
 
-		xy_out[i     ][0] += tempXY[0]
-		xy_out[i     ][1] += tempXY[1]
-	    }
-	    var w_XY_t = w_XY[0] * wphase_XY[0] - w_XY[1] * wphase_XY[1]; // rotate ... complex.h ?
-	    w_XY[1]    = w_XY[0] * wphase_XY[1] + w_XY[1] * wphase_XY[0]
-	    w_XY[0] = w_XY_t
-	}
-	mmax=istep
+                xy_out[i + mmax][0] = xy_out[i][0] - tempXY[0]
+                xy_out[i + mmax][1] = xy_out[i][1] - tempXY[1]
+
+                xy_out[i][0] += tempXY[0]
+                xy_out[i][1] += tempXY[1]
+            }
+
+            let w_XY_t = w_XY[0] * wphase_XY[0] - w_XY[1] * wphase_XY[1]
+            w_XY[1] = w_XY[0] * wphase_XY[1] + w_XY[1] * wphase_XY[0]
+            w_XY[0] = w_XY_t
+        }
+
+        mmax = istep
     }
+
     return xy_out
-  }
 }
