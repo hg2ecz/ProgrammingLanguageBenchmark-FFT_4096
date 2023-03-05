d08b1c14a4f6ce383f5ecb08b27392efe9824845
Zsolt Krüpl <hg2ecz@ham.hu>
2023-03-05 16:03:29 +0100

    Optimization

---
+++ Rust-tests/Rust-2023/src/fft.rs
@@ -0,0 +1,46 @@
+use num_complex::Complex;
+
+pub struct Fft {
+    phasevec: [Complex<f32>; 32],
+}
+
+// Public function
+impl Fft {
+    pub fn new() -> Fft {
+        const PHLEN: usize = 32;
+        let mut fft = Fft {
+            phasevec: [Complex::new(0.0, 0.0); PHLEN],
+        };
+        for i in 0..PHLEN {
+            let phase = -2.0 * std::f32::consts::PI / 2.0_f32.powi(i as i32 + 1);
+            fft.phasevec[i] = Complex::new(phase.cos(), phase.sin());
+        }
+        fft
+    }
+
+    pub fn fft(&self, xy_out: &mut [Complex<f32>; 4096], xy_in: &[Complex<f32>; 4096]) {
+        let log2point = xy_in.len().ilog2();
+        assert_eq!(xy_out.len(), xy_in.len());
+        assert_eq!(xy_in.len(), 1 << log2point);
+
+        xy_in
+            .iter()
+            .enumerate()
+            .for_each(|(i, &xy)| xy_out[i.reverse_bits() >> (usize::count_zeros(0) - log2point)] = xy);
+
+        // here begins the Danielson-Lanczos section;
+        for l2pt in 0..log2point {
+            let wphase_xy = self.phasevec[l2pt as usize];
+            let mmax = 1 << l2pt;
+            let mut w_xy = Complex::new(1.0, 0.0);
+            for m in 0..mmax {
+                for i in (m..xy_out.len()).step_by(mmax << 1) {
+                    let temp = w_xy * xy_out[i + mmax];
+                    xy_out[i + mmax] = xy_out[i] - temp;
+                    xy_out[i] += temp;
+                }
+                w_xy *= wphase_xy; // rotate
+            }
+        }
+    }
+}
---
+++ Rust-tests/Rust-2023/src/main.rs
@@ -0,0 +1,40 @@
+mod fft;
+
+use num_complex::Complex;
+use std::time::Instant;
+
+const LOG2FFTSIZE: u32 = 12;
+const FFT_REPEAT: u32 = 10000;
+const SIZE: usize = 1 << LOG2FFTSIZE;
+
+fn main() {
+    let mut xy = [Complex::new(0.0, 0.0); SIZE];
+    let mut xy_out_fft = [Complex::new(0.0, 0.0); SIZE];
+
+    for xy_act in xy.iter_mut().take(SIZE / 2) {
+        *xy_act = Complex::new(1.0, 0.0);
+    }
+    for xy_act in xy.iter_mut().take(SIZE).skip(SIZE / 2) {
+        *xy_act = Complex::new(-1.0, 0.0);
+    }
+
+    // FFT
+    let start_time = Instant::now();
+    let ffto = fft::Fft::new();
+    for _i in 0..FFT_REPEAT {
+        ffto.fft(&mut xy_out_fft, &xy)
+    }
+    let elapsed_time = start_time.elapsed();
+    let milliseconds = (elapsed_time.as_secs() as f64 * 1000.0) + (elapsed_time.subsec_nanos() as f64 / 1_000_000.0);
+
+    println!(
+        "{} piece(s) of {} pt FFT;    {} ms/piece\n\n",
+        FFT_REPEAT,
+        SIZE,
+        milliseconds / FFT_REPEAT as f64
+    );
+
+    for (i, &xy_act) in xy_out_fft.iter().enumerate().take(6) {
+        println!("{}  {} {}", i, xy_act.re, xy_act.im);
+    }
+}
