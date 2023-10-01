afd867fe11c080e08170e7be9f7908025b25951c
Zsolt Krüpl <hg2ecz@ham.hu>
2023-10-01 10:17:37 +0200

    +other_tests

--- Go/benchmark.go
+++ Go/benchmark.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+	"math"
+	"fft"
 )
 
 // Log2OffsetSize is the logarithm of number of points.
@@ -14,36 +16,31 @@ const FftRepeat int = 1000
 // Size is the number of points.
 const Size = (1 << Log2OffsetSize)
 
-func initializeInputArray() *[Size]complex128 {
-	var xy [Size]complex128
+func initializeInputArray() []complex64 {
+	xy := make([]complex64, Size)
 	var i int
-
 	for i = 0; i < Size/2; i++ {
-		xy[i] = (1. + 0i)
+		xy[i] = complex(float32(1.), 0.)
 	}
 	for ; i < Size; i++ {
-		xy[i] = (-1. + 0i)
+		xy[i] = complex(float32(-1.), 0.)
 	}
-
-	return &xy
+	return xy
 }
 
 func main() {
 	xy := initializeInputArray()
+	phaseVect := fft.InitializePhaseVector()
+	xyOutFft :=  make([]complex64, Size)
 	startTime := time.Now().UnixNano()
-	phaseVect := initializePhaseVector()
-
 	for j := 0; j < FftRepeat; j++ {
-		xyOutFft := Fft(Log2OffsetSize, xy, phaseVect)
-
-		if j == FftRepeat-1 {
-			var endTime = time.Now().UnixNano()
-
-			fmt.Printf("%8d piece(s) of %d pt FFT;    %.3f ms/piece\n\n", FftRepeat, Size, float64(endTime-startTime)/float64(1e6)/float64(FftRepeat))
-
-			for i := 0; i < 6; i++ {
-				fmt.Printf("%d %f %f\n", i, real(xyOutFft[i]), imag(xyOutFft[i]))
-			}
-		}
+		fft.Fft(Log2OffsetSize, xyOutFft, xy, phaseVect)
+	}
+	var endTime = time.Now().UnixNano()
+	fmt.Printf("%8d piece(s) of %d pt FFT;    %.3f ms/piece\n\n", FftRepeat, Size, float64(endTime-startTime)/float64(1e6)/float64(FftRepeat))
+	fmt.Printf("bin        real             imag           absval\n")
+	for i := 0; i < 6; i++ {
+		absval := math.Sqrt(float64(real(xyOutFft[i]) * real(xyOutFft[i]) + imag(xyOutFft[i]) * imag(xyOutFft[i])))
+		fmt.Printf("%3d %16.4f %16.4f %16.4f\n", i, real(xyOutFft[i]), imag(xyOutFft[i]), absval)
 	}
 }
