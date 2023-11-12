579765efa2582ce2c42df913b2e16a328b2edb0c
Fuszenecker Róbert <robert.fuszenecker@outlook.com>
2023-11-12 18:54:42 +0100

    U.

--- C#/DotnetBenchmark.cs
+++ C#/DotnetBenchmark.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Globalization;
 using System.Numerics;
-using System.Reflection;
-using System.Resources;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
